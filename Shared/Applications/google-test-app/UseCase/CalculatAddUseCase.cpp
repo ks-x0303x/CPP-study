874f6de33f63f20a9ad3d53e34b0e6e3b7a372c4
Seiya.K <seiya_1998@icloud.com>
2025-04-27 15:56:49 +0900

    デモ用App作成

---
+++ Shared/Applications/google-test-app/UseCase/CalculatAddUseCase.cpp
@@ -0,0 +1,1 @@
+#include "CalculatAddUseCase.hpp"
--- Shared/Applications/google-test-app/main.cpp
+++ Shared/Applications/google-test-app/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "Calculator.hpp"
+#include "CalculatAddUseCase.hpp"
 
 int main()
 {
-    std::cout << "main start up";
+    std::cout << "main start up" << std::endl;
+    std::shared_ptr<ICalculator> calculator = std::make_shared<Calculator>();
+    CalculatAddUseCase useCase(calculator);
+    int result = useCase.execute(3, 5);
+    std::cout << "Result of addition: " << result << std::endl;
+    std::cout << "main shut down" << std::endl;
+
     return 0;
 }
